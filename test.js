78c7e5f4fd3d11e597d9559b1a5c58c96d44e549
Yash Rajput <rajputyash8561@gmail.com>
2024-05-17 16:23:57 +0530

    array

--- test.js
+++ test.js
@@ -10,19 +10,24 @@
 // console.log(['10'] * ['10']); //100
 // console.log(['10'] + ['10']); //1010
 
-const promise1 = new Promise((resolve, reject) =>{
-    setTimeout(() =>{
-        resolve('promise 1');
-    }, 1000);
-});
+// const promise1 = new Promise((resolve, reject) =>{
+//     setTimeout(() =>{
+//         resolve('promise 1');
+//     }, 1000);
+// });
 
-const promise2 = new Promise((resolve, reject) =>{
-    setTimeout(() =>{
-        resolve('promise 2');
-    },2000); 
-});
+// const promise2 = new Promise((resolve, reject) =>{
+//     setTimeout(() =>{
+//         resolve('promise 2');
+//     },2000); 
+// });
 
-Promise.all([promise1, promise2])
-.then( value => {
-    console.log(value);
-});
+// Promise.all([promise1, promise2])
+// .then( value => {
+//     console.log(value);
+// });
+
+let x = 0.1 + 0.2;
+let y = 0.3;
+
+console.log(x == y);
